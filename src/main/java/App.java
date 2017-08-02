864cc807a1ee1d70ea67e27dfd702de8d166834c
Carson Canales and Evan Johnson <evan@>
2017-08-02 15:56:34 -0700

    guess full word and win

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -26,6 +26,22 @@ public class App {
                 System.out.println("Guess a letter!");
                 System.out.println("******************");
                 String input = bufferedReader.readLine().toLowerCase();
+                if (input.equals(gameWord)) {
+                    System.out.println("You win!!!!");
+                    System.out.println(gameWord);
+                    System.out.println("888                                                           \n" +
+                            "888                                                           \n" +
+                            "888                                                           \n" +
+                            "88888b.  8888b. 88888b.  .d88b. 88888b.d88b.  8888b. 88888b.  \n" +
+                            "888 \"88b    \"88b888 \"88bd88P\"88b888 \"888 \"88b    \"88b888 \"88b \n" +
+                            "888  888.d888888888  888888  888888  888  888.d888888888  888 \n" +
+                            "888  888888  888888  888Y88b 888888  888  888888  888888  888 \n" +
+                            "888  888\"Y888888888  888 \"Y88888888  888  888\"Y888888888  888 \n" +
+                            "                             888                              \n" +
+                            "                        Y8b d88P                              \n" +
+                            "                         \"Y88P\"                               ");
+                    programRunning = false;
+                }
                 if (thisGame.containsLetter(input)) {
                     thisGame.changeLetters(input);
                 } else {
