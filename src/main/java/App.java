e52c3e74ea822e034919187be1f74964b71867ed
Carson Canales and Evan Johnson <evan@>
2017-08-02 15:47:44 -0700

    add custom text picture at end of game

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -35,6 +35,17 @@ public class App {
 
                 if (thisGame.getWordAsArray().equals("finished")) {
                     System.out.println("Congratulations, you won!!! But you still stuck!");
+                    System.out.println("888                                                           \n" +
+                            "888                                                           \n" +
+                            "888                                                           \n" +
+                            "88888b.  8888b. 88888b.  .d88b. 88888b.d88b.  8888b. 88888b.  \n" +
+                            "888 \"88b    \"88b888 \"88bd88P\"88b888 \"888 \"88b    \"88b888 \"88b \n" +
+                            "888  888.d888888888  888888  888888  888  888.d888888888  888 \n" +
+                            "888  888888  888888  888Y88b 888888  888  888888  888888  888 \n" +
+                            "888  888\"Y888888888  888 \"Y88888888  888  888\"Y888888888  888 \n" +
+                            "                             888                              \n" +
+                            "                        Y8b d88P                              \n" +
+                            "                         \"Y88P\"                               ");
                     System.out.println(thisGame.getGameWord());
                     programRunning = false;
                 } else {
